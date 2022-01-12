9ee3dc17a7bbaaf19b672f3193bdc25b33e5622d
ngoclias <ngoc.leanh1999@gmail.com>
2022-01-12 09:29:52 +0700

    add expand Collapse animation

--- ng-animations-demo-start/src/app/zippy/zippy.component.ts
+++ ng-animations-demo-start/src/app/zippy/zippy.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { expandCollapse } from './zippy.component.animation';
 
 @Component({
   selector: 'app-zippy',
   templateUrl: './zippy.component.html',
   styleUrls: ['./zippy.component.css'],
+  animations: [expandCollapse],
 })
 export class ZippyComponent implements OnInit {
   @Input('title') title!: string;
